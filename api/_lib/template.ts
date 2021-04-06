aec97c85e8edb387281517dd1cd5855aa8d2e892
yoko <s1A121700885@toyo.jp>
2021-04-07 07:27:16 +0900

    fix

--- api/_lib/template.ts
+++ api/_lib/template.ts
@@ -18,84 +18,76 @@ const mono = readFileSync(`${__dirname}/../_fonts/Vera-Mono.woff2`).toString(
 
 function getCss() {
   return `
-    @import url('https://fonts.googleapis.com/css?family=Noto+Sans+JP');
-    @font-face {
-        font-family: 'Inter';
-        font-style:  normal;
-        font-weight: normal;
-        src: url(data:font/woff2;charset=utf-8;base64,${rglr}) format('woff2');
+  @import url('https://fonts.googleapis.com/css?family=Noto+Sans+JP');
+  @font-face {
+      font-family: 'Inter';
+      font-style:  normal;
+      font-weight: normal;
+      src: url(data:font/woff2;charset=utf-8;base64,${rglr}) format('woff2');
+  }
+  @font-face {
+      font-family: 'Inter';
+      font-style:  normal;
+      font-weight: bold;
+      src: url(data:font/woff2;charset=utf-8;base64,${bold}) format('woff2');
+  }
+  @font-face {
+      font-family: 'Vera';
+      font-style: normal;
+      font-weight: normal;
+      src: url(data:font/woff2;charset=utf-8;base64,${mono})  format("woff2");
     }
-    @font-face {
-        font-family: 'Inter';
-        font-style:  normal;
-        font-weight: bold;
-        src: url(data:font/woff2;charset=utf-8;base64,${bold}) format('woff2');
-    }
-    @font-face {
-        font-family: 'Vera';
-        font-style: normal;
-        font-weight: normal;
-        src: url(data:font/woff2;charset=utf-8;base64,${mono})  format("woff2");
-      }
-    body {
-        background: white;
-        border: solid 40px transparent;
-        border-image: url("https://www.infocrest.co.jp/ykxbdgs5qgq3/uploads/2019/10/blog_m02_2.jpg");
-        border-image-slice: 1;
-        border-image-repeat: stretch;
-        height: 90vh;
-        color:black;
-        margin:0px
-    }
-    .img{
-        font-family: 'Noto Sans JP', 'Inter', sans-serif;
-    }
-    .name{
-        font-size:60px;
-        font-weight: bold;
-        font-family: 'Noto Sans JP', 'Inter', sans-serif;
-        margin-right:50px
-    }
-    .flex{
-        margin-top:350px;
-        display:flex;
-        padding:50px
-    }
-    .logo{
-        font-size:60px;
-        font-weight: bold;
-        color:orange;
-        font-family: 'Noto Sans JP', 'Inter', sans-serif;
-        margin-right: auto;
-        margin-left:50px
-    }
-    code {
-        color: #D400FF;
-        font-family: 'Vera', 'M PLUS 1p';
-        white-space: pre-wrap;
-        letter-spacing: -5px;
-    }
-    code:before, code:after {
-        content: '\`';
-    }
+  body {
+      background: white;
+      border: solid 40px transparent;
+      border-image: url("https://www.infocrest.co.jp/ykxbdgs5qgq3/uploads/2019/10/blog_m02_2.jpg");
+      border-image-slice: 1;
+      border-image-repeat: stretch;
+      height: 90vh;
+      color:black;
+      margin:0px
+  }
+.wrapper{
+   display: flex;
+  align-items: center;
+  flex-direction: column;
+}
+  .img{
+      font-family: 'Noto Sans JP', 'Inter', sans-serif;
+  }
+  .name{
+      font-size:60px;
+      font-weight: bold;
+      font-family: 'Noto Sans JP', 'Inter', sans-serif;
+      margin-right:50px
+  }
+  .flex{
+display: flex;
+  justify-content: space-around;
+  width: 70%;
+      
+  }
+  .logo{
+     
+      font-weight: bold;
+      color:orange;
+      font-family: 'Noto Sans JP', 'Inter', sans-serif;
 
- 
-    .plus {
-        color: #BBB;
-        font-family: Times New Roman, Verdana;
-        font-size: 100px;
-    }
+      font-family: Times New Roman, Verdana;
+      font-size: 100px;
+  }
 
-    
-    .heading {
-        font-family: 'Noto Sans JP', 'Inter', sans-serif;
-        font-size: 125px;
-        font-style: normal;
-        font-weight: bold;
-        color: black;
-        padding:50px;
-        margin-left:20px
-    }`;
+  
+  .heading {
+      font-family: 'Noto Sans JP', 'Inter', sans-serif;
+      font-size: 125px;
+      font-style: normal;
+      font-weight: bold;
+      color: black;
+      padding:50px;
+      margin-left:20px
+      flex-grow: 1;
+  }`;
 }
 
 export function getHtml(parsedReq: ParsedRequest) {
@@ -109,7 +101,7 @@ export function getHtml(parsedReq: ParsedRequest) {
         ${getCss()}
     </style>
     <body>
-        <div>
+        <div class="wrapper">
             <div class="heading">${emojify(
               md ? marked(text) : sanitizeHtml(text)
             )}
