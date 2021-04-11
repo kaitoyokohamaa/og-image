2de4dc191ed265eaa648fae3de30f2ce405c736c
yoko <s1A121700885@toyo.jp>
2021-04-11 16:05:34 +0900

    fix

--- api/_lib/template.ts
+++ api/_lib/template.ts
@@ -43,7 +43,7 @@ function getCss() {
       border-image: url("https://www.infocrest.co.jp/ykxbdgs5qgq3/uploads/2019/10/blog_m02_2.jpg");
       border-image-slice: 1;
       border-image-repeat: stretch;
-      height: 94vh;
+      height: 93vh;
       color:black;
       margin:0px
   }
@@ -51,7 +51,7 @@ function getCss() {
    display: flex;
   align-items: center;
   flex-direction: column;
-min-height: 94vh;
+min-height: 93vh;
 }
   .img{
       font-family: 'Noto Sans JP', 'Inter', sans-serif;
